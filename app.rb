a8e4e36fa4c3439f953f6918ae1eb1ab11b2145a
fcuya13 <francocuya13@gmail.com>
2024-05-26 18:00:16 -0500

    finished initial setup

--- app.rb
+++ app.rb
@@ -8,11 +8,60 @@ set :bind, '0.0.0.0'
 set :port, 8080
 
 DB = Sequel.sqlite('db/app.db')
+Sequel::Model.plugin :json_serializer
+class Pelicula < Sequel::Model(DB[:peliculas])
+  one_to_many :pelicula_actores
+  one_to_many :pelicula_generos
+  many_to_many :actor, join_table: :pelicula_actor, right_key: :actor_id
+  many_to_many :genero, join_table: :pelicula_genero, right_key: :genero_id
+end
+
+class Sala < Sequel::Model(DB[:salas])
+end
+
+class PeliculaActor < Sequel::Model(DB[:pelicula_actor])
+  many_to_one :pelicula
+  many_to_one :actor
+end
+
+class PeliculaGenero < Sequel::Model(DB[:pelicula_genero])
+  many_to_one :pelicula
+  many_to_one :genero
+end
+
+class Actor < Sequel::Model(DB[:actor])
+  one_to_many :pelicula_actores
+  many_to_many :peliculas, join_table: :pelicula_actor, right_key: :pelicula_id
+end
+
+class Genero < Sequel::Model(DB[:genero])
+  one_to_many :pelicula_generos
+  many_to_many :peliculas, join_table: :pelicula_genero, right_key: :pelicula_id
+end
 
 get '/' do
   erb :home
 end
 
-get '/hola' do
-  'hola'
+get '/peliculas' do
+  content_type :json
+
+  peliculas = Pelicula.all.map do |pelicula|
+    {
+      id: pelicula.id,
+      titulo: pelicula.titulo,
+      sinopsis: pelicula.sinopsis,
+      imagen_url: pelicula.imagen_url,
+      trailer_url: pelicula.trailer_url,
+      actores: pelicula.actor.map(&:nombre),
+      generos: pelicula.genero.map(&:nombre)
+    }
+  end
+
+  peliculas.to_json
 end
+
+get '/salas' do
+  Sala.all.to_a.to_json
+end
+
