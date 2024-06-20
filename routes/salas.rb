f2c6c361dff998620d99937328bbfef70b8987d0
fcuya13 <francocuya13@gmail.com>
2024-06-20 10:03:04 -0500

    finished

--- routes/salas.rb
+++ routes/salas.rb
@@ -1,18 +1,35 @@
 require 'sinatra'
 
 
-# Configuración del middleware de JSON
-before do
-  content_type :json
-end
-
-# Ruta para obtener todas las salas usando una consulta SQL directa
 get '/salas' do
-  nombre = params['nombre']
-  if nombre
-    results = Sala.where(Sequel.ilike(:nombre, "%#{nombre}%")).all
+  content_type :json
+
+  if params[:id]
+    sala = Sala[params[:id]]
+
+    if sala
+      funciones = Funcion.where(sala_id: sala.id).map do |funcion|
+        {
+          id: funcion.id,
+          nombre_pelicula: funcion.pelicula.titulo,
+          time: funcion.fecha_hora,
+        }
+      end
+
+      {
+        id: sala.id,
+        titulo: sala.nombre,
+        sinopsis: sala.direccion,
+        imagen_url: sala.imagen_url,
+        funciones: funciones  
+      }.to_json
+    else
+      status 404
+      { message: "Movie with ID #{params[:id]} not found" }.to_json
+    end
   else
-    results = Sala.all #SELECT * FROM salas
+    sala = Sala.all
+
+    sala.to_json
   end
-  results.to_json
 end
